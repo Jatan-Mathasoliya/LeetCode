719a2a8ef452530f9b85a85db1964560ee928e6f
JATAN <jatan.codinggita@gmail.com>
2025-09-17 21:37:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (27.98%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -3,28 +3,27 @@ public:
     bool isPalindrome(string s) {
         string clean = "";
 
-        for (char c : s) {
-            if (isalnum(c)) {
+        for(char c : s){
+            if(isalnum(c)){
                 clean += tolower(c);
             }
         }
 
-        if (clean.length() == 0) {
+        int i = 0, j=clean.length()-1;
+
+        if(clean.length() == 0){
             return true;
         }
 
-        int i = 0, j = clean.length() - 1;
-
-        while (i < j) {
-           
-
-            if (clean[i] != clean[j]) {
+        while(i<j){
+            
+            if(clean[i] != clean[j]){
                 return false;
             }
+
             i++;
             j--;
         }
-
         return true;
     }
 };
