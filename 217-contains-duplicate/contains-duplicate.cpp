4f5b14a3937d0e5dc87d51626c5bfbb223661c4b
JATAN <jatan.codinggita@gmail.com>
2025-09-20 21:47:38 +0530

    Time: 47 ms (69.24%) | Memory: 90.9 MB (46.28%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -2,15 +2,13 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
 
-        unordered_map<int, int> freq;
+        unordered_set<int> seen;
 
         for (int num : nums) {
-            freq[num]++;
-
-            if (freq[num] > 1) {
-                cout << "num : " << num;
+            if(seen.find(num) != seen.end()){
                 return true;
             }
+            seen.insert(num);
         }
         return false;
     }
