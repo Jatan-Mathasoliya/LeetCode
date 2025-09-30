1f8281b7d9917f710ca3529a53f5a1d75e41e999
JATAN <jatan.codinggita@gmail.com>
2025-09-30 22:39:28 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (32.33%) - LeetSync

--- 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     int appendCharacters(string s, string t) {
         int i = 0, j = 0;
-
+        int temp = 0;
         while(i < t.length() && j < s.length()){
-            if(t[i] == s[j]){
+            if(t[i] == s[j]) {
+                temp += 1;
                 i++;
             }
             j++;
         }
+        cout << "Temp : " << temp;
 
-        if(i == 0) {
+        if(temp == t.length()) {
+            return 0;
+        }
+        else if(temp == 0){
             return t.length();
         }
-        else if(i == t.length()) return 0;
-
-        else return t.length() - i;
+        else return t.length() - temp;
     }
 };
