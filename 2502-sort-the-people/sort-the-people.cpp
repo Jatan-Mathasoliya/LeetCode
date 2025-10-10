8c41b362af4180a7eb467164958c2676c7c81110
JATAN <jatan.codinggita@gmail.com>
2025-10-10 22:44:28 +0530

    Time: 459 ms (5.07%) | Memory: 91.9 MB (5.23%) - LeetSync

--- 2502-sort-the-people/sort-the-people.cpp
+++ 2502-sort-the-people/sort-the-people.cpp
@@ -5,12 +5,16 @@ public:
             for(int j=i+1; j<heights.size(); j++){
                 if(heights[i] < heights[j]){
                     string temp = names[j];
-                    names[j] = names[i];
-                    names[i] = temp;
+                    // names[j] = names[i];
+                    // names[i] = temp;
 
-                    int temp1 = heights[j];
-                    heights[j] = heights[i];
-                    heights[i] = temp1; 
+                    // int temp1 = heights[j];
+                    // heights[j] = heights[i];
+                    // heights[i] = temp1; 
+
+                    swap(names[i], names[j]);
+                    swap(heights[i], heights[j]);
+
                 }
             }
         }
