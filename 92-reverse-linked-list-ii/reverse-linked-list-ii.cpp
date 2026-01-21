d860d2bce2bce8915e79113dd78c8a4d75abb39d
JATAN <jatan.codinggita@gmail.com>
2026-01-21 10:48:45 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (71.20%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -11,34 +11,58 @@
 class Solution {
 public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
+        // if(head == nullptr) return head;
 
-        if(head == nullptr) return head;
-
-        vector<int> arr;
+        // vector<int> arr;
         
-        ListNode* temp = head;
+        // ListNode* temp = head;
 
-        while(temp != nullptr){
-            arr.push_back(temp->val);
-            temp = temp->next;
+        // while(temp != nullptr){
+        //     arr.push_back(temp->val);
+        //     temp = temp->next;
+        // }
+
+        // int i=left-1, j=right-1;
+        // while(i <= j){
+        //     swap(arr[i], arr[j]);
+        //     i++;
+        //     j--;
+        // }
+
+        // ListNode* dummy = new ListNode(0);
+        // ListNode* curr = dummy;
+
+        // for(int i=0; i<arr.size(); i++){
+        //     curr->next = new ListNode(arr[i]);
+
+        //     curr = curr->next;
+        // }
+
+        // return dummy->next;
+
+        if(head == nullptr || head->next == nullptr) return head;
+
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* beforeLeft = &dummy;
+
+        for(int i=1; i<left; i++){
+            beforeLeft = beforeLeft->next;
         }
 
-        int i=left-1, j=right-1;
-        while(i <= j){
-            swap(arr[i], arr[j]);
-            i++;
-            j--;
+        ListNode* curr = beforeLeft->next;
+        ListNode* pre = nullptr;
+
+        for(int i=left  ; i<=right; i++){
+            ListNode* next = curr->next;
+            curr->next = pre;
+            pre = curr;
+            curr = next;
         }
 
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
+        beforeLeft->next->next = curr;
+        beforeLeft->next = pre;
 
-        for(int i=0; i<arr.size(); i++){
-            curr->next = new ListNode(arr[i]);
-
-            curr = curr->next;
-        }
-
-        return dummy->next;
+        return dummy.next;
     }
 };
